70475084b40e441c124ad1e5f8a7f8fefc4bf673
craftycram <github@craftycram.net>
2020-08-17 21:26:40 +0200

    cleaned up

--- src/main/java/net/craftycram/remcafkfishfarm/cmds/FishFarmCmd.java
+++ src/main/java/net/craftycram/remcafkfishfarm/cmds/FishFarmCmd.java
@@ -1,9 +1,5 @@
 package net.craftycram.remcafkfishfarm.cmds;
 
-import com.github.steveice10.mc.protocol.data.message.Message;
-import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
-import com.sasha.reminecraft.ReMinecraft;
-import com.sasha.reminecraft.util.TextMessageColoured;
 import me.someonelove.bettercommandsystem.Command;
 
 public class FishFarmCmd extends Command {
